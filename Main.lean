993df5e5bd754674b13a4fbbad2b138b97e72674
Satsuki Akiba <anosatsuk124@gmail.com>
2024-09-28 13:05:22 +0900

    Update

--- Main.lean
+++ Main.lean
@@ -3,18 +3,24 @@ import MusicCompositionLang
 open MusicCompositionLang
 
 def main : IO Unit := do
-    IO.println $ Score.toMmlCode $ [Note.Pitch (Pitch.Upper Pitch.zero) default, Note.Pitch Pitch.zero (Duration.num 2).Dot]
     let scale := [0, 2, 3, 5, 7, 8, 10].map (fun x => PitchClass.fromNat x)
       |> Scale.fromPitchClasses
     IO.println $ "Scale: " ++ toString scale
 
     let score := [
       0, 1, 2, 3, 4, 5, 6
-    ] |>.map (fun x => Note.Pitch (Pitch.Upper (Pitch.fromNat x)) (Duration.num 4)) |> Score.mk
+    ] |>.map (fun x => Note.Pitch (Pitch.fromNat x) (Duration.num 4)) |> Score.mk
 
     let inScaleScore := score |>.toInScaleScore scale |>.toScore
 
-    IO.println $ "Scale: \n" ++ inScaleScore.toMmlCode
+    IO.println $ inScaleScore.toMmlCode
+
+    let scoreUpperOctave := score |>.map (fun note => match note with
+      | Note.Pitch pitch duration => Note.Pitch (Pitch.Upper pitch) duration
+      | n => n
+    ) |> Score.mk
+
+    IO.println $ scoreUpperOctave.toMmlCode
 
 #eval Note.Pitch Pitch.zero (Duration.num 2).Dot |>.toMmlCode
 
